ac8b6be53815b2a6c292b49b25d0d9d09f9a8bb7
Gabriel Ruelas <40619710+gruelasjr@users.noreply.github.com>
2025-12-03 19:28:25 -0600

    Satisfy updated response factory interface in tests

--- tests/Support/FakeResponseFactory.php
+++ tests/Support/FakeResponseFactory.php
@@ -14,9 +14,19 @@ class FakeResponseFactory implements ResponseFactory
         return new JsonResponse($content, $status, $headers);
     }
 
+    public function noContent($status = 204, array $headers = [])
+    {
+        return new JsonResponse(null, $status, $headers);
+    }
+
     public function json($data = [], $status = 200, array $headers = [], $options = 0)
     {
         return new JsonResponse($data, $status, $headers, $options);
+    }
+
+    public function jsonp($callback, $data = [], $status = 200, array $headers = [], $options = 0)
+    {
+        return new JsonResponse(['callback' => $callback, 'data' => $data], $status, $headers, $options);
     }
 
     public function view($view = null, $data = [], $status = 200, array $headers = [])
@@ -42,5 +52,30 @@ class FakeResponseFactory implements ResponseFactory
     public function file($file, array $headers = [])
     {
         return $this->json(['file' => $file], 200, $headers);
+    }
+
+    public function redirectTo($path, $status = 302, $headers = [], $secure = null)
+    {
+        return $this->json(['redirect' => $path, 'secure' => $secure], $status, (array) $headers);
+    }
+
+    public function redirectToRoute($route, $parameters = [], $status = 302, $headers = [])
+    {
+        return $this->json(['route' => $route, 'parameters' => $parameters], $status, $headers);
+    }
+
+    public function redirectToAction($action, $parameters = [], $status = 302, $headers = [])
+    {
+        return $this->json(['action' => $action, 'parameters' => $parameters], $status, $headers);
+    }
+
+    public function redirectGuest($path, $status = 302, $headers = [], $secure = null)
+    {
+        return $this->json(['redirect' => $path, 'secure' => $secure, 'guest' => true], $status, (array) $headers);
+    }
+
+    public function redirectToIntended($default = '/', $status = 302, $headers = [], $secure = null)
+    {
+        return $this->json(['redirect' => $default, 'secure' => $secure, 'intended' => true], $status, (array) $headers);
     }
 }
